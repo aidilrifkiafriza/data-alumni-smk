fadcc2973a8215f4aa6b3ff003e6022f89cdb09c
Aidil <aidilrifki2@gmail.com>
2025-02-25 10:36:09 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
-document.addEventListener("DOMContentLoaded", function () {
-    document.getElementById("login-button").addEventListener("click", login);
-});
+// Data login (username & password)
+const validUsername = "admin";
+const validPassword = "12345";
+
+function login() {
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+
+    if (username === validUsername && password === validPassword) {
+        document.getElementById("login-container").style.display = "none";
+        document.getElementById("alumni-container").style.display = "block";
+    } else {
+        document.getElementById("login-error").innerText = "Username atau password salah!";
+    }
+}
